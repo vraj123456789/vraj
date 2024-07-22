c60cfc2f713ec3c8440726b07e8160a5d31ff9d9
vraj123456789 <vraj9023215049@gmail.com>
2024-07-22 08:53:50 +0530

    Add files via upload

---
+++ 2.1   1  .c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int a=6,b=12;
+	printf("%d+%d=%d",a,b,a+b);
+}
---
+++ 2.1___3__.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int a=12,b=6;
+	printf("%d*%d=%d",a,b,a*b);
+}
