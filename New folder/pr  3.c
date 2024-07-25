32ff10b2bf38643e3ef7694f5cda1cabdbb0e2e9
vraj123456789 <vraj9023215049@gmail.com>
2024-07-25 18:33:27 +0530

    Add files via upload

---
+++ New folder/pr  3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int a,b,c;
+	printf("enter a:");
+	scanf("%d",&a);
+	printf("enter b:");
+	scanf("%d",&b);
+	c=180-(a+b);
+	printf("ans=%d",c);
+}
+	
