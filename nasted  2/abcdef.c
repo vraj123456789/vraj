dd33af49d582964fd4f04ebcce142834c0e2cc6f
vraj123456789 <vraj9023215049@gmail.com>
2024-08-07 12:31:47 +0530

    Add files via upload

---
+++ nasted  2/abcdef.c
@@ -0,0 +1,365 @@
+#include<stdio.h>
+
+void main()
+{
+	int a,b,c,d,e,f;
+	
+   	printf("enter a:");
+	scanf("%d",&a);
+	printf("enter b:");
+	scanf("%d",&b);
+	printf("enter c:");
+	scanf("%d",&c);
+	printf("enter d:");
+	scanf("%d",&d);
+	printf("enter e:");
+	scanf("%d",&e);
+	printf("enter f:");
+	scanf("%d",&f);
+	
+	//a,b,c,d,e,f,
+	if(a>b)
+	{
+		if(a>c)
+		{
+			if(a>d)
+			{
+				if(a>e)
+				{
+					if(a>f)
+					{
+						printf("a is max");	
+					}
+					else
+					{
+						printf("f is max");	
+					}		
+				}
+				else
+				{
+					if(e>f)
+					{
+						printf("e is max");
+					}
+					else
+					{
+						printf("f is max");
+					}
+				}
+			}
+			else
+			{
+				if(d>e)
+				{
+					if(d>f)
+					{
+						printf("d is max");
+					}
+					else
+					{
+						printf("f is max");
+					}
+				}
+				else
+				{
+					if(e>f)
+					{
+						printf("e is max");	
+					}
+					else
+					{
+						printf("f is max");	
+					}
+				}
+			}
+		}
+		else
+		{
+			if(c>d)
+			{
+				if(c>e)
+				{
+					if(c>f)
+					{
+						printf("c is max");	
+					}
+					else
+					{
+						printf("f is max");
+					}
+				}
+				else
+				{
+					if(e>f)
+					{
+						printf("e is max");	
+					}
+					else
+					{
+						printf("f is max");	
+					}
+				}	
+			}
+			else
+			{
+				if(d>e)
+				{
+					if(d>f)
+					{
+						printf("d is max");
+					}
+					else
+					{
+						printf("f is max");
+					}
+				}
+				else
+				{
+					if(e>f)
+					{
+						printf("e is max");
+					}
+					else
+					{
+						printf("f is max");
+					}
+				}
+			}
+		}
+	}
+	else
+	{
+		if(b>c)
+		{
+			if(b>d)
+			{
+				if(b>e)
+				{
+					if(b>f)
+					{
+						printf("b is max");
+					}
+					else
+					{
+						printf("f is max");	
+					}
+				}
+				else
+				{
+					if(c>d)
+					{
+						if(c>e)
+						{
+							if(c>f)
+							{
+								printf("c is max");
+							}
+							else
+							{
+								printf("f is max");
+							}
+						}
+						else
+						{
+							if(d>e)
+							{
+								if(d>f)
+								{
+									printf("d is max");
+								}
+								else
+								{
+									printf("f is max");
+								}
+							}
+							
+						}	
+					}
+					else
+					{
+						if(d>e)
+						
+						{
+							if(d>f)
+							{
+								printf("d is max");
+							}
+							else
+							{
+								printf("f is max");
+							}
+						}
+						else
+						{
+							if(e>f)
+							{
+								printf("e is max");
+							}
+							else
+							{
+								printf("f is max");
+							}
+						}
+					}	
+				}
+			}
+			else
+			{
+				if(d>e)
+				{
+					if(d>f)
+					{
+						printf("d is max");
+					}
+					else
+					{
+						printf("f is max");
+					}
+				}
+				else
+				{
+					if(e>f)
+					{
+						printf("e is max");
+					}
+					else
+					{
+						printf("f is max");
+					}
+				}
+			}
+		}
+		else
+		{
+			if(c>d)
+			{
+				if(c>e)
+				{
+					if(c>f)
+					{
+						printf("c is max");	
+					}
+					else
+					{
+						printf("f is max");
+					}
+				}
+				else
+				{
+					if(e>f)
+					{
+						printf("e is max");
+					}
+					else
+					{
+						printf("f is max");
+					}
+				}
+			}
+			else
+			{
+				if(d>e)
+				{
+					if(d>f)
+					{
+						printf("d is max");	
+					}
+					else
+					{
+						printf("f is max");	
+					}
+			    }
+				else
+				{
+					if(e>f)
+					{
+						printf("e is max");
+					}
+					else
+					{
+						printf("f is max");	
+					}
+				}
+			}
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
