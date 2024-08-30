ad91bbe4dbe7e78915621a22c9c190cbe35e4bd2
vraj123456789 <vraj9023215049@gmail.com>
2024-08-30 21:25:21 +0530

    Add files via upload

---
+++ loop/1 to  n.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+main()
+{
+	int n,i;
+	printf("enter the value:");
+	scanf("%d",&n);
+	
+	i=1;
+	while(i<=n)
+	{
+		printf("%d\n",i);
+		i++;
+	}
+	
+	
+	
+}
---
+++ loop/n to 1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+main()
+{
+	int n,i;
+	printf("enter the value:");
+	scanf("%d",&n);
+	
+	i=1;
+	while(i<=n)
+	{
+		printf("%d\n",n);
+		n--;
+	
+	}	
+}
