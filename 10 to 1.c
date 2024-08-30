ff16aa7ebeaaf9d7bf170f096e14af6e7fdb91d4
vraj123456789 <vraj9023215049@gmail.com>
2024-08-30 18:48:16 +0530

    Add files via upload

---
+++ 10 to 1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+main()
+{
+	int i;
+
+	i=10;
+	while(i>=1)
+	{
+		printf("%d\n",i);
+		i--;
+	}
+	
+	
+	
+}
---
+++ leap year.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+main()
+{
+	int start=2000,end;
+	
+	
+	end=3000;
+	while(start<=end)
+	{
+		if(start%4==0)
+		{
+			printf("%d\n",start);
+		}
+		start++;
+	}
+	
+	
+	
+}
